086520619587a204df2762b158b76a1e96d590c7
anarmh <anarmh@code.edu.az>
2022-10-24 05:33:35 -0700

    add extension method for factorial

---
+++ Extention/Factorial/Factorial/Helpers/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factorial.Helpers
+{
+   public static class Extension
+    {
+        public static int Factorial(this int mult,int a)
+        {
+            for (int i = a; i > 1; i--)
+            {
+                mult *= i;
+            }
+            return mult;
+        }
+    }
+}
---
+++ Extention/Factorial/Factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using Factorial.Helpers;
+using System;
+
+namespace Factorial
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int mult = 1;
+            Console.WriteLine(mult.Factorial(4));
+        }
+    }
+}
