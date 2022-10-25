162664ec7898f39c3882754218cc070b699926f2
anarmh <anarmh@code.edu.az>
2022-10-25 20:45:28 +0400

    An app that checks the login according to the role

---
+++ Login verification by role/Login verification by role/Login verification by role/Program.cs
@@ -0,0 +1,18 @@
+﻿using Service.Helpers;
+using Service.Implementation;
+using System;
+
+namespace Login_verification_by_role
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account account = new Account();
+
+            account.Login("test@code.edu.az ", "test12345", 1);
+            
+        }
+    }
+}
---
+++ Login verification by role/Login verification by role/Service/Implementation/Account.cs
@@ -0,0 +1,30 @@
+﻿using Service.Helpers;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Implementation
+{
+    public class Account : IAccount
+    {
+        
+
+        public void Login (string email,string password,int roleId)
+        {
+            if(email!= "test@code.edu.az " || password!= "test12345")
+            {
+                Console.WriteLine("please correct the email or password");
+            }
+            if(roleId != (int)Roles.SuperAdmin)
+            {
+                Console.WriteLine("You do not have permission to enter");
+            }
+            if (email == "test@code.edu.az " && password == "test12345" && roleId == (int)Roles.SuperAdmin)
+            {
+                Console.WriteLine("login is successful");
+            }
+            return;
+        }
+    }
+}
